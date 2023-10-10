875a14cf275f045dbcec344ccc2e0787a1c2f917
Vssblt <sunruiyangcp01@gmail.com>
2023-10-10 16:18:54 +0800

    删除dpi对进度条的影响

--- html5/js/lib/circle-progress.js
+++ html5/js/lib/circle-progress.js
@@ -222,12 +222,12 @@
       canvas.height = this.size;
       this.ctx = canvas.getContext('2d');
 
-      if (window.devicePixelRatio > 1) {
-        var scaleBy = window.devicePixelRatio;
-        canvas.style.width = canvas.style.height = this.size + 'px';
-        canvas.width = canvas.height = this.size * scaleBy;
-        this.ctx.scale(scaleBy, scaleBy);
-      }
+      // if (window.devicePixelRatio > 1) {
+      //   var scaleBy = window.devicePixelRatio;
+      //   canvas.style.width = canvas.style.height = this.size + 'px';
+      //   canvas.width = canvas.height = this.size * scaleBy;
+      //   this.ctx.scale(scaleBy, scaleBy);
+      // }
     },
 
     /**
